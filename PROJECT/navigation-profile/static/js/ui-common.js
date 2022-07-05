017553b062a54e131652c72ed227de5a03675a4c
SeungY <7718jkcoo@gmail.com>
2022-07-05 17:55:18 +0900

    ajax study update / project update

---
+++ PROJECT/navigation-profile/static/js/ui-common.js
@@ -0,0 +1,8 @@
+// layer-lock
+let btnActive = document.querySelector('.btn-login');
+let layerActive = document.querySelector('.layer-lock')
+btnActive.addEventListener('click', function() {
+	layerActive.classList.add('active');
+	// 7월 6일 할거 className 도 만들어보기, input error 부분 수정
+});
+
