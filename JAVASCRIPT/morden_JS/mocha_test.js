74d3ee77a2b6a9cbfd840f2d87a9b3b85f17fa38
SeungY <7718jkcoo@gmail.com>
2022-09-21 14:06:20 +0900

    2022-09-20 study update

--- JAVASCRIPT/morden_JS/mocha_test.js
+++ JAVASCRIPT/morden_JS/mocha_test.js
@@ -22,7 +22,7 @@ describe("pow", function() {
 		makeText(x);
 	}
 
-	// 중첩 describe
+	// 중첩 describe (위 pow와 같은 동작을 실행)
 	describe('x를 세번 곱합니다.', function() {
 		function makeText2(x) {
 			let expected2 = x * x * x;
@@ -37,7 +37,8 @@ describe("pow", function() {
 	});
 });
 
-describe('test', function() {
+/* 대개 초기화 용도로 사용
+describe('test', function() { 
 	before(() => alert('테스트를 시작 - 시작 전'));
 	after(() => alert('테스트 종료 - 종료 후'));
 
@@ -46,4 +47,46 @@ describe('test', function() {
 
 	it('test 1', () => alert(1));
 	it('test 2', () => alert(2));
+}); */
+
+describe('pow', function() { // n이 조건에 맞지않을때 NaN를 반환하는지 아닌지 검사
+
+	it('n이 음수일때 결과는 NaN입니다', function() {
+		assert.isNaN(pow(2, -1));
+	});
+
+	it('n이 정수가 아닐 때 결과는 NaN입니다', function() {
+		assert.isNaN(pow(2, 1.5));
+	});
 });
+
+// 예제
+// 아래 테스트 코드는 잘못되었다 (문법오류는 없고 테스트는 문제없이 통과함)
+describe('pow_test', function() {
+	it('주어진 숫자의 n 제곱', function() {
+		let x = 5;
+		let result = x;
+		assert.equal(pow(x, 1), result);
+
+		result *= x;
+		assert.equal(pow(x, 2), result);
+
+		result *= x;
+		assert.equal(pow(x, 3), result);
+	});
+});
+
+// 위 함수식은 3개지만 실행하는 동작은 하나이고 복잡하게 되어있기때문에 it구문을 분리해서 적는것이 좋다
+describe('주어진 숫자의 n제곱', function() {
+	it('5를 1제곱하면 5', function() {
+		assert.equal(pow(5, 1), 5);
+	});
+	// Mocha는 아래 블록만 실행합니다.
+	// it.only('5를 2제곱하면 25', function() {
+	// 	assert.equal(pow(5, 2), 25);
+	// });
+	it('5를 3제곱하면 125', function() {
+		assert.equal(pow(5, 3), 125);
+	});
+});
+
