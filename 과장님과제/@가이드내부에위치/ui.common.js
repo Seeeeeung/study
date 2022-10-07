5db352d3cad25c4742ef3d717f3d70da22f44608
SeungY <7718jkcoo@gmail.com>
2022-10-07 17:46:40 +0900

    2022-10-07 navhighlighter 수정중

--- 과장님과제/@가이드내부에위치/ui.common.js
+++ 과장님과제/@가이드내부에위치/ui.common.js
@@ -68,20 +68,19 @@ $(function() {
 				}
 			});
 
-			// 좌우탭 버튼 클릭시 북마크 좌우 위치 이동
+			// 좌우탭 버튼 클릭시 북마크이너탭 좌우 위치 이동
 			bookmark.find('button').each(function() {
 				console.log($(this).hasClass('prev'))
 				$(this).on('click', function(e) {
-					var getBmInnerLocation = bmInner.scrollLeft();
+					let currentBmInner = bmInner.scrollLeft();
 					switch(true) {
 						case $(this).hasClass('prev'):
-							getBmInnerLocation -= bookmark.innerWidth()
+							bmInner.scrollLeft(currentBmInner -= bookmark.innerWidth());
 							break;
 						case $(this).hasClass('next'):
-							getBmInnerLocation += bookmark.innerWidth()
+							bmInner.scrollLeft(currentBmInner += bookmark.innerWidth());
 							break;
 					}
-
 					tabScrollX();
 				});
 			});
@@ -111,25 +110,81 @@ $(function() {
 					// 2. 클릭한 버튼과 연동된 해당 리스트의 top에서부터의 위치 찾기
 					// 3. 해당 리스트의 위치에서 헤더높이값 빼기
 					allMenu.scrollTop(gnb.children('.gnb-list').children('li#'+linkId).offset().top - fixHeight + 1);
+					console.log(bmInner.scrollLeft())
 				});
 			}
-			console.log(bmInner.prop('scrollWidth'))
-			console.log(bmInner[0].scrollWidth + ' : width')
-			console.log(bmInner[0].offsetWidth + ' : width')
-			console.log($(document).innerWidth())
-			console.log($(window).innerWidth())
-			console.log($(document).innerWidth() + $(window).innerWidth())
-			console.log($(this).scrollLeft())
+
 			// 좌우 버튼 활성화, 비활성화
 			function tabScrollX() {
-				var bmIScrollT = bmInner.scrollTop()
-				var bmIW = bmInner.height();
-				bmlnner.scrollLeft() == 0 ? bookmark.find('.prev').addClass('hide') : bookmark.find('.prev').removeClass('hide');
-				// bmlInner.scrollLeft() == bmInner.scrollWidth - bmIW ? bookmark.find('.next').addClass('hide') : bookmark.find('.next').removeClass('hide');
+				bmInner.scrollLeft() == 0 ? bookmark.find('.prev').addClass('hide') : bookmark.find('.prev').removeClass('hide');
+				bmInner.scrollLeft() >= bmInner[0].scrollWidth - bmInner.scrollLeft() ? bookmark.find('.next').addClass('hide') : bookmark.find('.next').removeClass('hide');
 			}
 
+			// 상하 스크롤 이동시 탭이너 하이라이트
+			function navHighlighter() {
+				allMenu.on('scroll', function() {
+					var scrollY = allMenu.scrollTop();
 
-			// bmInner.on('scroll', tabScrollX);
+					scrollY >= gnb.offset().top - (fixHeight - bookmark.height()) ? bookmark.addClass('active') : bookmark.removeClass('active');
+
+					sections.each(function() {
+						var sectionHeight = $(this).height(),
+						sectionTop = $(this).offset().top - fixHeight,
+						sectionId = $(this).attr('id');
+	
+						// 리스트 위치가 안맞아서 스크롤과 버튼 클릭 기능이 다르게작동함
+						// ** 여기서부터 다시 하기
+						console.log(sectionTop)
+
+						if (scrollY < gnb.offset().top) {
+							bmInner.eq(0).addClass('active');
+							console.log('작동')
+						} else if (scrollY > sectionTop - 30 && scrollY <= sectionTop + sectionHeight + 15) {
+							// 1. 현재 리스트의 아이디 찾기 -> sectionId
+							console.log(sectionId + ' : 리스트 아이디')
+							// 2. 1번과 같은 탭메뉴 버튼의 href찾기
+							// 2-1. 탭메뉴버튼 전체 href 속성 반환
+							const bmHref = bmInner.children().map(function() {
+								return $(this).attr('href').split('#')[1]
+							})
+							console.log(bmHref)
+							// 2-2. 그중에 리스트 아이디와 같은것 반환
+							for (let i = 0; i < bmHref.length; i++) {
+								if (bmHref[i] == sectionId) {
+
+									bmInner.children().removeClass('active')
+									bmInner.children().eq(i).addClass('active')
+								} 
+							}
+							//2-3. 그 속성을 가진 객체 반환 .... 결론적으로 그 href의 객체를 반환하려면 검색을 해야하는데 어캐함
+							// 다른방법이 있나
+							// 인덱스 연동시키기?
+
+
+							// 3. 찾은 버튼에 active 추가하기
+							// 4. 나머지 버튼에 active제거하기
+
+							
+							bmInner.scrollLeft(gnb.find('.gnb-tab a.active').offset().left - parseInt(bmInner.attr('padding-left')))
+						} else {
+							// gnb.children('.gnb-tab').find('a').attr('href', sectionId).removeClass('active');
+							// console.log($(this))
+						}
+					});
+
+				});
+
+
+				
+				console.log(bookmark.height())
+				console.log(gnb.offset().top)
+				console.log(scrollY)
+
+				tabScrollX();
+			}
+
+			bmInner.on('scroll', tabScrollX());
+			allMenu.on('scroll', navHighlighter());
 		}
 	}
 }); 
