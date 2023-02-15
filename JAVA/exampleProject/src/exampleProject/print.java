805c1ff6fa29cc2141b57e719611ba1ace1b5e5f
KimSeungYeon <7718jkcool@gmail.com>
2023-02-15 10:17:14 +0900

    02-14 java 공부시작

---
+++ JAVA/exampleProject/src/exampleProject/print.java
@@ -0,0 +1,9 @@
+package exampleProject;
+
+public class print {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!!");
+		System.out.println("2+3 = 5");
+		System.out.println(2+3);
+	}
+}
